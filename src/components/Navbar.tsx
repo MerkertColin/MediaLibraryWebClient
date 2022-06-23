074d40a30281e7ad358bc56ac9f1fb2a09cc183f
Colin Merkert <c.merkert@outlook.de>
2022-06-23 17:38:04 +0200

    adding basic navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,20 @@
+import { FunctionComponent } from "react";
+import '../styles/Navbar.css';
+
+interface NavbarProps {
+    
+}
+ 
+const Navbar: FunctionComponent<NavbarProps> = () => { 
+    return ( 
+        <nav>
+            <ul>
+                <li><a href='#'>Books</a></li>
+                <li><a href='#'>Games</a></li>
+                <li><a href='#'>Movies</a></li>
+            </ul>
+        </nav>
+     );
+}
+ 
+export default Navbar;
